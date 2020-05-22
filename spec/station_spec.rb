204e082038195daebfa9ee4416d1ec239f2dd341
Tim <TimCPB@github.com>
2020-05-22 14:37:54 +0100

    refactored station.rb to include attr_reader

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -8,12 +8,12 @@ describe Station do
     expect(station).to be_instance_of Station
   end
 
-  it "initializes with a @name instance variable" do
-    expect(station.instance_variable_get(:@name)).to eq("Brixton")
+  it "knows its name" do
+    expect(station.name).to eq("Brixton")
   end
 
-  it "initializes with @zone instance variable" do
-    expect(station.instance_variable_get(:@zone)).to eq(2)
+  it "knows its zone" do
+    expect(station.zone).to eq(2)
   end
 
 end
